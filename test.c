b2b4efb778d3728ac3f9727dd770e4dce4a13ba8
TimothySyu <a0960622153@gmail.com>
2017-08-15 22:38:12 +0800

    second commit
    chpater1

--- test.c
+++ test.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+
+int Fac(int n);
+int main()
+
+{
+	int number,result;
+
 	printf("please enter a number:");
+	scanf(" %d",&number);
+	
+	result=Fac(number);
+	
+	printf("Facteria(%d)=%d\n",number,result);
+	
+	return 0;
+
+}
+
+int Fac(int n)
+{
+	if(n==0)
+		return 1;
+	else 
+		return(Fac(n-1)*n);
+
+}

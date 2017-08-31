31859e9339260a44208d0b1fe12a17cff2dccb52
TimothySyu <a0960622153@gmail.com>
2017-09-01 00:13:15 +0800

    fix a033

--- basic_coding_test/a033.c
+++ basic_coding_test/a033.c
@@ -5,10 +5,18 @@
 int main(int argc, char const *argv[])
 {
 	int number;
+
 	scanf("%d",&number);
 
-	printf("|%4.8d|\n",number );
-
-
+	if (number<10)
+		printf("|_______%d|\n",number );
+	else if(number<100)
+		printf("|______%d|\n",number);
+	else if (number<1000)
+		printf("|_____%d|\n",number );
+	else if (number<10000)
+		printf("|____%d|\n",number );
+	else
+		printf("|____%04d|\n",number%10000);
 	return 0;
 }
